a632c912f89bcc17ee4f65ad288643db9266174a
Maksym Popov <80689833+maksympm@users.noreply.github.com>
2023-11-08 22:15:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
 // const type = typeof (те, що визначаємо); - повертає на своє місце тип даних і записує його в змінну
 // console.log(type) - виводить в консоль результат роботи методу
 
-// Виведення вкні з повідомленням
+// Виведення вікна з повідомленням
 // alert('Повідомлення');
 
 // Метод window.confirm() - показує діалогове вікно з кнопками ок і cancel
@@ -23,3 +23,27 @@
 // let quantity = prompt('Введіть кількість товарів');
 // quantity = Number(quantity); - перетворення введеного на число
 // console.log(typeof quantity); - визначення типу даних
+
+// Метод Number.parseInt() - зліва посимвольно парсить рядок зупиняючись на першому не числі(не деструктивний метод)
+// let elementWidth = '50px'; - отримуємо рядок з числом і літерами
+// const result = Number.parseInt(elementWidth); - робимо змінну в яку присвоюється результат роботи методу parseInt
+// console.log('elementWidth:', result); - виводимо результат в консоль
+
+// Метод Number.parseFloat() - зліва посимвольно парсить рядок до першого не числа не враховуючи крапку
+// let elementHeight = '200.74px'; - отримуємо рядок з числом і літерами
+// elementHeight = Number.parseFloat(elementHeight); - перевизначаємо значення за допомогою методу Number.parseFloat()
+// console.log(elementHeight); - виводимо результат в консоль
+
+// Метод toFixed() - відсікає вказану в дужках кількість символів після крапки у вигляді рядка
+// let salary = 1300.16472; - отримуємо число з крапкою
+// salary = salary.toFixed(2); - застосовуємо метод
+// salary = Number(salary); - перетворюємо на число
+// console.log(salary); - виводимо результат в консоль
+
+// Об'єкт Math
+// Зведення в ступінь, метод pow
+// const base = 2;
+// const power = 5;
+// const result = Math.pow(base, power);
+// console.log(result)
+
